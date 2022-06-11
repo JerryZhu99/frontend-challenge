8accea30853dec26269ede2f525be2c092a5fce3
Jerry Zhu <jerryzhu99@gmail.com>
2022-06-10 22:05:33 -0700

    Improve dropdowns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import './App.css';
-import Dropdown from './components/Dropdown';
-import { useState } from 'react';
+import "./App.css";
+import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState([]);
+  const [value1, setValue1] = useState();
+  const [value2, setValue2] = useState([]);
+  const [value3, setValue3] = useState([]);
   return (
     <div className="App">
-      Example
       <div className="content">
-        <Dropdown placeholder="Test" options={["123", "345"]} value={value} onChange={v => setValue(v)} multiple>
-        </Dropdown>
+        <h1>Examples</h1>
+        <h2>Single Select Dropdown</h2>
+        <Dropdown
+          placeholder="Select an item"
+          options={["a", "b", "c", "d", "e"]}
+          value={value1}
+          onChange={(v) => setValue1(v)}
+        />
+        <code>value: {JSON.stringify(value1)}</code>
+        <h2>Multiple Select Dropdown</h2>
+        <Dropdown
+          placeholder="Select items"
+          options={["a", "b", "c", "d", "e"]}
+          value={value2}
+          onChange={(v) => setValue2(v)}
+          multiple
+        />
+        <code>value: {JSON.stringify(value2)}</code>
+        <h2>Large List</h2>
+        <Dropdown
+          placeholder="Select items"
+          options={new Array(10000).fill(0).map((_, i) => `${i}`)}
+          value={value3}
+          onChange={(v) => setValue3(v)}
+          multiple
+        />
+        <code>value: {JSON.stringify(value3)}</code>
       </div>
     </div>
   );
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,67 +1,100 @@
 import React, { useMemo, useState } from "react";
-import PropTypes from 'prop-types';
-import DropdownOption from "./DropdownOption";
+import PropTypes from "prop-types";
 
-import "./Dropdown.css"
+import "./Dropdown.css";
 
-function Dropdown (props) {
-    const [expanded, setExpanded] = useState(false);
-    const { placeholder, value, options, onChange, multiple } = props;
+function Dropdown(props) {
+  const [expanded, setExpanded] = useState(false);
+  const { placeholder, value, options, onChange, multiple } = props;
 
-    const selectedValues = useMemo(() => {
-        return new Set(value);
-    }, [value])
+  const selectedValues = useMemo(() => {
+    return new Set(value);
+  }, [value]);
 
-    const selectionEmpty = !value || value.length === 0;
-    const selectedLabel = multiple ? value.join(", ") : value;
-    const dropdownLabel = selectionEmpty ? placeholder : selectedLabel;
+  const selectionEmpty = !value || value.length === 0;
+  const selectedLabel = multiple ? value.join(", ") : value;
+  const dropdownLabel = selectionEmpty ? placeholder : selectedLabel;
 
-    let optionsClassName = "dropdown-options";
-    if (expanded) optionsClassName += " expanded";
+  let optionsClassName = "dropdown-options";
+  if (expanded) optionsClassName += " expanded";
 
-    const optionClickHandler = (value) => {
-        if (!multiple) {
-            return onChange(value);
-        }
+  const dropdownClickHandler = () => setExpanded(!expanded);
 
-        const newValues = new Set(selectedValues);
-        if (selectedValues.has(value)) {
-            newValues.delete(value);
-        } else {
-            newValues.add(value);
-        }
-        return onChange([...newValues]);
+  const optionAllHandler = () => {
+    onChange(selectedValues.size === options.length ? [] : options);
+  }
+  
+  const optionNoneHandler = () => {
+    setExpanded(false);
+    onChange();
+  }
+
+  const optionClickHandler = (value) => {
+    if (!multiple) {
+      setExpanded(false);
+      return onChange(value);
     }
 
-    return (
-        <div className="dropdown">
-            <button className="dropdown-button" onClick={() => setExpanded(!expanded)}>
-                {dropdownLabel} {expanded ? "▴" : "▾"}
-            </button>
-            {expanded && <div className={optionsClassName}>
-                {options.map(option => {
-                    let {value, label} = option;
+    const newValues = new Set(selectedValues);
+    if (selectedValues.has(value)) {
+      newValues.delete(value);
+    } else {
+      newValues.add(value);
+    }
+    return onChange([...newValues]);
+  };
 
-                    const selected = selectedValues.has(value);
-                    if (typeof option === "string") {
-                        value = option;
-                        label = option;
-                    }
+  return (
+    <div className="dropdown">
+      <button className="dropdown-button" onClick={dropdownClickHandler}>
+        {dropdownLabel} {expanded ? "▴" : "▾"}
+      </button>
+      {expanded && (
+        <div className={optionsClassName}>
+          {multiple ? (
+            <div className="dropdown-option" onClick={optionAllHandler}>
+              Select All
+            </div>
+          ) : (
+            <div className="dropdown-option" onClick={optionNoneHandler}>
+              None
+            </div>
+          )}
+          {options.map((option) => {
+            let { value, label } = option;
 
-                    return (
-                        <DropdownOption key={value} value={value} selected={selected} label={label} onClick={optionClickHandler}/>
-                    );
-                })}
-            </div>}
+            if (typeof option === "string") {
+                value = option;
+                label = option;
+            }
+
+            const selected = selectedValues.has(value);
+
+            let optionClassName = "dropdown-option";
+            if (selected) optionClassName += " selected";
+
+            return (
+              <div
+                className={optionClassName}
+                key={value}
+                onClick={() => optionClickHandler(value)}
+              >
+                {label}
+              </div>
+            );
+          })}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 Dropdown.propTypes = {
-    placeholder: PropTypes.string,
-    options: PropTypes.array,
-    onChange: PropTypes.func,
-    multiple: PropTypes.bool,
-}
+  placeholder: PropTypes.string,
+  value: PropTypes.any,
+  options: PropTypes.array,
+  onChange: PropTypes.func,
+  multiple: PropTypes.bool,
+};
 
 export default Dropdown;
